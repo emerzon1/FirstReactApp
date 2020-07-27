14be8626c4990d12c856df40e57adae6d037a6c3
Evan Merzon <evandaniel160@yahoo.com>
2020-07-27 17:51:04 -0400

    Add build folder

---
+++ build/precache-manifest.4598d0b6f9a74c4479eb30cd2d53ffca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9cfe94a478685719a7455d7131f4468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05b616a304c5a5e9ce39",
+    "url": "/static/js/2.e1eef825.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1eef825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5a6ca42ce07a129961",
+    "url": "/static/js/main.f259d790.chunk.js"
+  },
+  {
+    "revision": "e958ebdfb318d4d8990c",
+    "url": "/static/js/runtime-main.72bbb588.js"
+  }
+]);
